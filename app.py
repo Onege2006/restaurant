aaf3c2a9724215bccd0498b532c2521aaadbb111
Onege <onege@MacBook-Pro-Akniyet.local>
2025-04-11 16:48:04 +0500

    Обновление Procfile и app.py

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template, request, redirect, url_for
 import sqlite3
-
+import os
 app = Flask(__name__)
 
 # Словари переводов
@@ -147,5 +147,6 @@ def reserve():
     return redirect(url_for('index'))
 
 
-if __name__ == '__main__':
-    app.run(debug=True)
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5001))
+    app.run(host="0.0.0.0", port=port, debug=True)
